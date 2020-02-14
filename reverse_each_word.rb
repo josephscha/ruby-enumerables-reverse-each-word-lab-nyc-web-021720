f246822dd68fd9c2fb27c83c04dcdebb38ed0be7
Joseph S. Cha <josephshincha@gmail.com>
2020-02-14 01:40:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(source)
-  array1 = source.split("")
+  array1 = source.split(" ")
   index = 0 
   reversed_array = []
   reversed_array = array1.collect {|word| word.reverse} 
   
-  reversed_array.join("")
+  reversed_array.join(" ")
 end 
