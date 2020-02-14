00e7890e4411e8900f3c1312f7bc1c35ef926c8e
Joseph S. Cha <josephshincha@gmail.com>
2020-02-14 01:40:48 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word(source)
+  array1 = source.split("")
+  index = 0 
+  reversed_array = []
+  reversed_array = array1.collect {|word| word.reverse} 
+  
+  reversed_array.join("")
+end 
